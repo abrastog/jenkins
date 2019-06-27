aa3674e06e2f79d2b42b7a5fe149067f0d53ed1e
abrastog <abhayrastogi26.1991@gmail.com>
2019-06-27 02:11:10 +0000

    yo

--- user_check.sh
+++ user_check.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-set -x
+#set -x
 
-echo -n "checking user is present or not: "
+echo  "checking user is present or not!! "
 #USER_NAME="root"
-echo $USER_NAME
+echo -n \n"Enter the user name here:-->>"
+read USER_NAME
 ##############################################
 #                                            #
 #        checking user is present or not     #
 #                                            #
 ##############################################
-cat /etc/passwd | grep -i $USER_NAME | cut -d: -f 1
+CHECK=$(cat /etc/passwd | grep -i $USER_NAME | cut -d: -f 1)
 
-if [[ $? == 0 ]]
+if [[ $CHECK == $USER_NAME ]]
 then
    echo "user is present "
-else [[ $? == 1 ]]
+else 
    echo "user is not available"
 fi
