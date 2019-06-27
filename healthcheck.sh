6eff320b6fd4582b0ac869f032a8fbb12b75cad5
abrastog <abhayrastogi26.1991@gmail.com>
2019-06-27 04:16:55 +0000

    yo

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/bash
 echo -n "Hostname: " $HOSTNAME
 
-echo "HERE i am checking user input\n" $1
+echo ""
+
+echo "HERE i am checking user input\n" 
+$1
+sudo su - abhay 
 sudo yum install nfs  -y
 figlet "abhay-world"
 df -h
