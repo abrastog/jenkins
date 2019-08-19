4149eae44c3e5ca83ea4fb9c149ddcd72b24544a
abrastog <abhayrastogi26.1991@gmail.com>
2019-08-19 13:02:44 +0000

    aa

--- user_check.sh
+++ user_check.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 #set -x
-#ssh -t -t $HOSTNAME
-echo $USER_NAME
+USER_NAME=$1
 hostname
 echo  "checking user is present or not!! "
-#USER_NAME="root"
-#echo -n \n"Enter the user name here:-->>"
-#read USER_NAME
 ##############################################
 #                                            #
 #        checking user is present or not     #
@@ -17,16 +13,10 @@ CHECK=$(cat /etc/passwd | grep -i "$USER_NAME" | cut -d: -f 1)
 
 if [ -z "$CHECK" ]
 then
-      echo "\$CHECK is empty"
+      echo "User  is not present"
 else
-      echo "\$CHECK user is present"
+      echo "$CHECK user is present"
 fi
 
 
 
-#if [[ $CHECK == $USER_NAME ]]
-#then
-#   echo "user is present "
-#else 
-#   echo "user is not available"
-#fi
